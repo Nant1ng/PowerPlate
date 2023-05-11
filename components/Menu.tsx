c25a62a6d90dd6bab67e46b6819fcc359a025828
Andrés <andressantana99@hotmail.se>
2023-05-11 17:00:46 +0200

    Added some style

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -19,6 +19,26 @@ const MenuText = styled.h2`
   margin-bottom: 1rem;
   color: #4a4a4a;
   cursor: pointer;
+  position: relative;
+  ::after {
+    content: "";
+    position: absolute;
+    width: 100%;
+    transform: scaleX(0);
+    height: 2px;
+    bottom: 0;
+    left: 0;
+    background-color: #000000;
+    transform-origin: center;
+    transition: transform 0.25s ease-out;
+  }
+  :hover::after {
+    transform: scaleX(1);
+    transform-origin: center;
+  }
+  :hover {
+    color: #000000;
+  }
 `;
 
 function Menu() {
